61c568855f31cf985e5311fcd8b24edb03e6402b
Weronika Jasinska <werjas@wp.pl>
2022-04-30 18:26:30 +0200

    Trzecia wersja

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import math as mt
 
-def silnia(n_k):
-  n_k = n_k.split(" ")
+def silnia(liczby):
+  n_k = liczby.split(" ")
   n_k_tablica_int = [int(x) for x in n_k]
   n = n_k_tablica_int[0]
   k = n_k_tablica_int[1]
-  return mt.factorial(n)/(mt.factorial(k)*mt.factorial(n-k))
-
-
+  wynik = mt.factorial(n)/(mt.factorial(k)*mt.factorial(n-k))
+  print(wynik)
 
 silnia(input())
+
